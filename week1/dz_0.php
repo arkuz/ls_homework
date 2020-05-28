057e0fb4c78e5958614b07dd36402c2715152eca
Artem Kuzmenko <arkuzka@mail.ru>
2020-05-28 13:31:16 +0300

    Добавил условия задач в виде комментариев к коду

--- week1/dz_0.php
+++ week1/dz_0.php
@@ -1,5 +1,16 @@
 <?php
 
+/*
+Приведите этот код к соответствию стандарту PSR-2
+<?php
+CONST testConstanta = 'asd';
+$user_name = "Igor";
+
+function show_something(){ RETURN 'SOMETHING';}
+
+if (1==1) echo 'hi';
+*/
+
 const TEST_CONSTANTA = 'asd';
 $userName = 'Igor';
 
