7f8d65d125ce0608e5bd896d28d4847586ecab24
Artem Kuzmenko <arkuzka@mail.ru>
2020-06-01 21:40:14 +0300

    Добавил Задание #3

---
+++ week2/dz_3/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require("src/functions.php");
+
+echo '<b>Задание 3.1</b><br>';
+$users = task3_1();
+echo "<pre>";
+print_r($users);
+echo "</pre>";
+echo '<p>';
+// -------------------------------------------------
---
+++ week2/dz_3/src/functions.php
@@ -0,0 +1,83 @@
+<?php
+
+function getRandomName(): string
+{
+    $names = [
+        'Мария',
+        'Сергей',
+        'Игорь',
+        'Марина',
+        'Артем'
+    ];
+    $idx = rand(0, sizeof($names) - 1);
+    return $names[$idx];
+}
+
+
+function generateUsersArray(int $startPos = 0, int $finishPos = 50): array
+{
+    for ($i = $startPos; $i < $finishPos; $i++) {
+        $users[] = [
+            'id' => $i,
+            'name' => getRandomName(),
+            'age' => rand(18, 45),
+        ];
+    }
+    return $users;
+}
+
+
+function saveToFileAsJSON(string $filename, array $arr)
+{
+    file_put_contents($filename, json_encode($arr));
+}
+
+
+function openJSONFileAsArray(string $filename)
+{
+    return json_decode(file_get_contents($filename), true);
+}
+
+
+function getCountNames(array $arr): array
+{
+    $namesCount = [];
+    foreach ($arr as $key => $value) {
+        if (!array_key_exists($value['name'], $namesCount)) {
+            $namesCount[$value['name']] = 0;
+        }
+        $namesCount[$value['name']] += 1;
+    }
+    return $namesCount;
+}
+
+
+function getAverageAge(array $arr): float
+{
+    $count = 0;
+    $sum = 0;
+    foreach ($arr as $key => $value) {
+        foreach ($value as $item => $val) {
+            if ($item == 'age') {
+                $sum += $val;
+                $count++;
+            }
+        }
+    }
+    return round($sum / $count);
+}
+
+
+function task3_1()
+{
+    $users = generateUsersArray();
+    $filename = 'users.json';
+
+    saveToFileAsJSON($filename, $users);
+    $loadedUsers = openJSONFileAsArray($filename);
+
+    return [
+        'names' => getCountNames($loadedUsers),
+        'averageAge' => getAverageAge($loadedUsers),
+    ];
+}
