1d5afd728468d7f491e60999bd30c7c9f7dc3ac8
Artem Kuzmenko <arkuzka@mail.ru>
2020-06-19 22:10:01 +0300

    Обновил config_example

--- week3/my_mvc/config_example.php
+++ week3/my_mvc/config_example.php
@@ -4,3 +4,9 @@ const DB_HOST = '';
 const DB_NAME = '';
 const DB_USER = '';
 const DB_PASSWORD = '';
+
+/**
+ * 'twig' = Native PHP template
+ * 'any_text' = Twig template
+ */
+const TEMPLATE = '';
